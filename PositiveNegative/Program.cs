6d427497d29d5f8244ba44ca7fa4aa688a2be2bd
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-09 11:13:47 +0300

    PositiveNegative

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kasutajal palub  sisestada numbrit -10 kuni 10;
+            //programm kontrollib numbrit;
+            //kui number on positiivne, konsool kuvab "positive";
+            //kui number on negatiivne, konsool kuvab "negative";
+            //kui number on 0, siis konsool kuvab "null";
+
+            Console.WriteLine("Palun sisesta arv -10 kuni 10");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(UserNumber > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else if (UserNumber < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+
+
+        }
+    }
+}
